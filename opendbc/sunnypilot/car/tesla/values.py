71dd895ca9287ebf9aab8aa761ce9ec3a864d7a3
dzid26 <dzidmail@gmail.com>
2025-12-10 21:43:58 +0000

    Test nonlinear Tesla steering angle mapping hack

--- opendbc/sunnypilot/car/tesla/values.py
+++ opendbc/sunnypilot/car/tesla/values.py
@@ -6,6 +6,10 @@ See the LICENSE.md file in the root directory for more details.
 """
 from enum import IntFlag
 
+class TeslaAngleMap:
+  # angle request will be scaled by this map
+  XP = [0, 90, 200, 330, 450]  # corresponds to model desired angle
+  YP = [0, 90, 180, 270, 360]  # corresponds to steering command
 
 class TeslaFlagsSP(IntFlag):
   HAS_VEHICLE_BUS = 1  # 3-finger infotainment press signal is present on the VEHICLE bus with the deprecated Tesla harness installed
